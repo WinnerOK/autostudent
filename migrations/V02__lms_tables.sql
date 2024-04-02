13a5f4ea5dade0264e2390298689a7f36680b32f
MrNikulin <leonidnikulin2001@gmail.com>
2024-04-02 15:24:40 +0300

    Add lms tables

---
+++ migrations/V02__lms_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS autostudent.courses (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS autostudent.lessons (
+  id BIGSERIAL PRIMARY KEY,
+  course_id BIGSERIAL REFERENCES autostudent.courses (id) ON DELETE CASCADE NOT NULL,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS autostudent.youtube_summarization (
+  youtube_video_url VARCHAR(100) PRIMARY KEY,
+  lesson_id BIGSERIAL REFERENCES autostudent.lessons (id) ON DELETE CASCADE NOT NULL,
+  summarization TEXT
+);
