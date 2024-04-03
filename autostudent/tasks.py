b59b51b2d65b21d23802bb1a1b05d9a94349ca8d
CHAAAPS <vitars2001@gmail.com>
2024-04-03 15:14:08 +0300

    send_notifications fix, import fix

--- autostudent/tasks.py
+++ autostudent/tasks.py
@@ -2,8 +2,9 @@ from typing import Annotated
 
 import asyncpg
 from taskiq import TaskiqDepends
+from telebot.async_telebot import AsyncTeleBot
 
-import parser
+import autostudent.parser as parser
 from autostudent.broker import db_pool_dep, bot_dep, broker
 from autostudent.repository.subscription import get_course_subscribers
 
@@ -20,12 +21,15 @@ async def process_courses_lessons_job(
 
 @broker.task
 async def send_notifications(
-    course_id: int, db_pool: Annotated[asyncpg.Pool, TaskiqDepends(db_pool_dep)]
+    course_id: int,
+    db_pool: Annotated[asyncpg.Pool, TaskiqDepends(db_pool_dep)],
+    bot: Annotated[AsyncTeleBot, TaskiqDepends(bot_dep)],
 ):
     conn: asyncpg.Connection
-    bot = bot_dep()
+    print("send")
     async with db_pool.acquire() as conn:
         subs = await get_course_subscribers(conn, course_id)
+        print(subs)
         for sub in subs:
             await bot.send_message(
                 sub,
