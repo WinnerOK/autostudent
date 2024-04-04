30b8115dc348ddfe0edc08703b852f1302f6c0e4
WinnerOK <danman339@gmail.com>
2024-04-04 12:36:59 +0300

    fix notifications

--- autostudent/notification_task.py
+++ autostudent/notification_task.py
@@ -3,6 +3,10 @@ from typing import Annotated
 import asyncpg
 from taskiq import TaskiqDepends
 from telebot.async_telebot import AsyncTeleBot
+from telebot.types import LinkPreviewOptions
+from telebot.util import smart_split
+import telebot.formatting as fmt
+
 
 from autostudent.broker import db_pool_dep, broker, bot_dep
 from autostudent.repository.subscription import get_course_subscribers
@@ -23,9 +27,12 @@ async def send_notifications(
     async with db_pool.acquire() as conn:
         subs = await get_course_subscribers(conn, course_id)
         md = markdown_keypoints(video_url, summary)
-        msg = f'Появилась новая лекция по "{course_name}": "{lecture_name}"\nСуммаризация:\n' + md
+        msg = f'Появилась новая лекция по "{fmt.escape_markdown(course_name)}": "{fmt.escape_markdown(lecture_name)}"\nСуммаризация:\n' + md
         for sub in subs:
-            await bot.send_message(
-                sub,
-                msg,
-            )
+            for part in smart_split(msg):
+                await bot.send_message(
+                    sub,
+                    part,
+                    parse_mode='MarkdownV2',
+                    link_preview_options=LinkPreviewOptions(is_disabled=True)
+                )
